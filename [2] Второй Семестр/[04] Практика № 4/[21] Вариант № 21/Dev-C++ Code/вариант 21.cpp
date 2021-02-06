45c340156bb3a5a5e825b72de3083368babea683
Vladimir K <vkalininz@mail.ru>
2021-02-06 12:46:26 +0300

    Create вариант 21.cpp

---
+++ [2] Второй Семестр/[04] Практика № 4/[21] Вариант № 21/Dev-C++ Code/вариант 21.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int protection_n () {
+	int value;
+	while(!(cin>>value)) {
+		cout<<"Не верные данные! Пожалуйста вводите только цифры! \n";
+		cin.clear();
+		_flushall();
+	}
+	return value;
+}
+
+int main () {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int *array_A = new int[10];
+
+	int *N = new int(0);
+
+	for (int i = 0; i < 10; i++) {
+		cout << "A[" << i << "] : ";
+		*(array_A + i) = protection_n();
+		if ( *(array_A + i) > 0 ) {
+			(*N)++;
+		}
+	}
+
+	int *array_B = new int[*N];
+
+	for (int i = 0, j = 0; i < 10; i++) {
+		if ( *(array_A + i) > 0 ) {
+			*(array_B + j) = *(array_A + i);
+			j++;
+		}
+	}
+
+	cout << "\nМассив array_A \n";
+	for (int i = 0; i < 10; i++) {
+		cout << *(array_A + i) << " \t";
+	}
+	cout << "\nМассив array_B \n";
+	for (int i = 0; i < *N; i++) {
+		cout << *(array_B + i) << " \t";
+	}
+
+	delete[] array_A;
+	delete[] array_B;
+	delete N;
+}
