8524a92ed340d6e8dbc7123dce02836a01bab3b3
Vladimir K <vkalininz@mail.ru>
2021-02-06 12:50:09 +0300

    Create вариант 47.cpp

---
+++ [2] Второй Семестр/[04] Практика № 4/[47] Вариант № 47/Dev-C++ Code/вариант 47.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int protection_arr () {
+	int value;
+	while(!(cin>>value && value >= 1)) {
+		cout<<"Не верные данные! Пожалуйста вводите только положительные цифры больше 0! \n";
+		cin.clear();
+		_flushall();
+	}
+	return value;
+}
+
+int protection_n () {
+	int value;
+	while(!(cin>>value)) {
+		cout<<"Не верные данные! Пожалуйста вводите только цифры! \n";
+		cin.clear();
+		_flushall();
+	}
+	return value;
+}
+
+int main () {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	int *N = new int;
+	int *M = new int;
+	int *multiply = new int(1);
+	int *Z = new int;
+
+	cout << "N : ";
+	*N = protection_arr();
+
+	cout << "M : ";
+	*M = protection_arr();
+
+
+	int **array_A = new int*[*N];
+
+	for (int i = 0; i < *N; i++) {
+		*(array_A + i) = new int[*M];
+	}
+
+	for (int i = 0; i < *N; i++) {
+		for (int j = 0; j < *M; j++) {
+			cout << "array_A[" << i << "][" << j << "] : ";
+			*(*(array_A + i) + j) = protection_n();
+		}
+	}
+
+	if ( *M > *N ) {
+		*Z = *N;
+	} else if ( *M < *N ) {
+		*Z = *M;
+	} else {
+		*Z = *N;
+	}
+
+	for (int i = 0; i < *Z; i++) {
+		*multiply *= *(*(array_A + i) + i);
+	}
+
+	cout << "Произведение элементов, расположенных на главной диагонали : " << *multiply;
+
+	for (int i = 0; i < *N; i++) {
+		delete *(array_A + i);
+	}
+	delete[] array_A;
+	delete multiply;
+	delete N;
+	delete M;
+}
