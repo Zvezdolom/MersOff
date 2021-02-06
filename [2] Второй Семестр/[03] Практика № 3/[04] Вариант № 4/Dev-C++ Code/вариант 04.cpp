a3c39ba04398bb38553bcded49b06e7ec3c65940
Vladimir K <vkalininz@mail.ru>
2021-02-06 12:33:43 +0300

    Create вариант 04.cpp

---
+++ [2] Второй Семестр/[03] Практика № 3/[04] Вариант № 4/Dev-C++ Code/вариант 04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main () {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	FILE *in;
+	in=fopen("file04.txt","r");
+	char c;
+	int words = 1;
+	fread(&c, 1, 1, in);
+	while (!feof(in)) {
+		if (c == ' ' || c == '\n') {
+			words++;
+		}
+		fread(&c, 1, 1, in);
+	}
+	cout << "Всего слов " << words;
+	fclose(in);
+}
